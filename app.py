5f12a66cd2a4813e1ab007922f14ab0ce6896a19
anas-aljanaby <anas.aljanaby00@gmail.com>
2024-07-07 12:18:19 +0300

    add new chat button

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ VERBOSE = 1
 with open("config.yaml", "r") as file:
     config = yaml.safe_load(file)
 
+
+def initialize_handler():
+    return RAGHandler(verbose=VERBOSE, add_context=True)
+
+
 if "handler" not in st.session_state:
     rag_handler = RAGHandler(verbose=VERBOSE, add_context=True)
     st.session_state["handler"] = rag_handler
@@ -15,6 +20,11 @@ st.title("Okta AI - chat with local files")
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+if st.sidebar.button("New Chat"):
+    st.session_state.messages = []
+    st.session_state["handler"] = initialize_handler()
+    st.rerun()
+
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
@@ -22,6 +32,7 @@ for message in st.session_state.messages:
 uploaded_files = st.sidebar.file_uploader(
     "Upload", label_visibility="collapsed", accept_multiple_files=True
 )
+
 for file in uploaded_files:
     st.session_state["handler"].retriever.save_embedding(file)
 
